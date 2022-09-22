623af9843c8d09b0b7e9e4997ad6716f85eb8f50
Brian Lambert <brianlambert2408@gmail.com>
2022-09-22 21:43:02 +1000

    Completed tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const tasks = [];
+const priorityStyles ={
+  1: "red",
+  2: "yellow",
+  3: "green"
+};
+
+const form = document.querySelector("#create-task-form")
+const todoEl = document.querySelector("#tasks");
+
+form.addEventListener("submit", e=>{
+  e.preventDefault();
+
+  if(!newTaskDescription.value){ return; }
+
+  const priority = !!newTaskPriority.value ? newTaskPriority.value : 1;
+
+  tasks.push({
+    value: newTaskDescription.value, 
+    priority: parseInt(priority),
+    date: newTaskDate.value,
+  });
+
+  form.reset();
+
+  renderTasks();
 });
+
+function renderTasks(){
+  const sortedTasks = [...tasks].sort((a,b)=> a.priority - b.priority);
+
+  todoEl.innerHTML = "";
+
+  sortedTasks.forEach(task=>{
+    todoEl.innerHTML += `
+      <li class="${priorityStyles[task.priority]} list-item">
+        <div class="task">
+          <div class="desc">${task.value}</div>
+          <div class="date">${task.date}</div>
+        </div>
+        <div>
+          <button class="btn-delete">x</button>
+        </div>
+      </li>`;
+  });
+  //update button events
+  //delete
+  document.querySelectorAll("li .btn-delete").forEach(btn =>{
+    btn.addEventListener("click", e => {
+      e.target.parentElement.parentElement.remove();
+    });
+  });
+}
+
+
+function todoRemove(e){
+  const parent = e.target.parentElement;
+  parent.remove();
+}
